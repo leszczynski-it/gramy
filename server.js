132d132c9f889911330e3583e657f9af2c43cdc8
Piotr Leszczynski <piotr@leszczynski.it>
2016-03-16 16:36:12 +0100

    streams with rx.js

--- server.js
+++ server.js
@@ -3,19 +3,28 @@ var app = express();
 var server = require('http').createServer(app);
 var config = require('config');
 var io = require('socket.io')(server);
+var Rx = require('rx');
+
+var subject = new Rx.ReplaySubject(1);
 
 app.use('/public', express.static(__dirname + '/public'));
 
 var port = config.get('server.port');
+
 server.listen(port, function () {
-  console.log('Example app listening on port ' + port + '!');
+  console.log('Server listening on port ' + port + '!');
 });
 
 app.get('/update/song', function(req, res) {
-   io.emit('data_update', {data: 'abc'});
+   var data = {data: 'abc'};
+   subject.onNext(data);
    res.status(200).end(); 
 });
 
 io.on('connection', function(socket){
   console.log('a user connected');
+  
+  subject.subscribe(function (data) {
+    socket.emit('data_update', data);
+  });
 });
